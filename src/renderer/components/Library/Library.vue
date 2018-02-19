b6b3d2e741ba85e7788ede66b09da8d0e90e57d8
Eric Gardner <eric@crake.local>
2018-02-19 11:47:55 -0800

    Build out basic Library components

--- src/renderer/components/Library/Library.vue
+++ src/renderer/components/Library/Library.vue
@@ -1,23 +1,28 @@
 <template>
   <div>
-    <h1>Test Test</h1>
+    <h1>Standard Library</h1>
     <h2>Books</h2>
-    <ul>
+    <ul v-if="ready">
       <li v-for="book in books">
-        <h1>{{ book.title }}</h1>
-        <h2>{{ book.authors }}</h2>
+        <library-item :book="book"></library-item>
       </li>
     </ul>
+    <div v-else>
+      Loading...
+    </div>
   </div>
 </template>
 
 <script>
 import BooksService from '@/services/BooksService'
+import LibraryItem from '@/components/Library/LibraryItem'
 export default {
-  name: 'library',
+  name      : 'library',
+  components: { LibraryItem },
   data () {
     return {
-      books: []
+      books: [],
+      ready: false
     }
   },
   created () {
@@ -27,11 +32,16 @@ export default {
     fetchBooks () {
       return BooksService.get().then(response => {
         this.books = response
+        this.ready = true
       })
     }
   }
 }
 </script>
 
-<style>
+<style scoped>
+  li {
+  border: solid 1px #333;
+  margin-bottom: 1rem;
+  }
 </style>
--- src/renderer/components/Library/LibraryItem.vue
+++ src/renderer/components/Library/LibraryItem.vue
@@ -1,48 +1,24 @@
 <template>
-<div>
-  <a :href="epubLink">
-    <img v-if="thumbnailLink" :src="thumbnailLink"/>
-    <h3>{{ book.title }}</h3>
-    <h4>{{ authors }}</h4>
-    <p>{{ book.summary._ }}</p>
-  </a>
-</div>
+  <div>
+    <img alt="" :src="book.thumbnail"/>
+    <h1>{{ book.title }}</h1>
+    <ul>
+      <li v-for="author in book.authors">{{ author.name }}</li>
+    </ul>
+    <p>
+      {{ book.summary }}
+    </p>
+    <h3>Categories</h3>
+    <ul>
+      <li v-for="category in book.categories">{{ category }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-  name    : 'library-item',
-  props   : ['book'],
-  computed: {
-    authors () {
-      if (Array.isArray(this.book.author)) {
-        return this.book.author.map(i => { return i.name })
-      } else {
-        return this.book.author.name
-      }
-    },
-    thumbnailLink () {
-      if (this.book.link.length > 0) {
-        let link = this.book.link.find(l => {
-          return l.$.rel === 'http://opds-spec.org/image/thumbnail'
-        })
-        return `https://standardebooks.org/${link.$.href}`
-      } else {
-        return false
-      }
-    },
-    epubLink () {
-      if (this.book.link.length > 0) {
-        let link = this.book.link.find(l => {
-          return l.$.type === 'application/epub+zip' &&
-            l.$.href.match(/(\.epub$)/)
-        })
-        return `https://standardebooks.org/${link.$.href}`
-      } else {
-        return false
-      }
-    }
-  }
+  name : 'library-item',
+  props: ['book']
 }
 </script>
 
