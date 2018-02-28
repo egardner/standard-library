c3d9980ae2ccb2ef575d3029ac04cbbd246968c5
Eric Gardner <eric@crake.local>
2018-02-28 09:26:06 -0800

    Bulma-ify left-hand panel

--- src/renderer/components/Library/Library.vue
+++ src/renderer/components/Library/Library.vue
@@ -4,19 +4,29 @@
     <div class="library__main">
       <div class="library__main__panel"
            :class="{ 'is-visible': panelVisible }">
-        <div v-if="selectedView === 'authors'">
-          <h1 class="title">Authors</h1>
+        
+        <div v-if="selectedView === 'authors'" class="panel">
+          <div class="panel-heading">
+            <h1 class="title is-4">Authors</h1>
+          </div>
           <ul>
-            <li v-for="author in authors">
-              {{ author }}
+            <li v-for="author in authors" class="">
+              <a href="" class="panel-block">
+                {{ author }}
+              </a>
             </li>
           </ul>
         </div>
-        <div v-else-if="selectedView === 'subjects'">
-          <h1 class="title">Subjects</h1>
+        
+        <div v-else-if="selectedView === 'subjects'" class="panel">
+          <div class="panel-heading">
+            <h1 class="title is-4">Subjects</h1>
+          </div>
           <ul>
             <li v-for="subject in subjects">
-              {{ subject }}
+              <a href="#" class="panel-block">
+                {{ subject }}
+              </a>
             </li>
           </ul>
         </div>
@@ -76,13 +86,17 @@ export default {
     width: 0%;
     
     &.is-visible {
-      border-right: solid 1px $grey-lighter;
+      // border-right: solid 1px $grey-lighter;
       flex: 25%;
       -webkit-overflow-scroll: touch;
       overflow-y: scroll;
-      padding: 1rem;
+      // padding: 1rem;
       max-width: 25%;
       width: 25%;
+    }
+
+    .panel-heading {
+      border-radius: 0;
     }
   }
   
