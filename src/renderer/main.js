0ad54735e15004013b510bc8a7e5a1ebadf87a65
Eric Gardner <eric@crake.local>
2018-03-01 09:30:39 -0800

    Cache book data offline with LocalStorage
    Uses Vuex's store.subscribe() method to persist book data in localStorage.
    Following the guide here:
    https://www.mikestreety.co.uk/blog/vue-js-using-localstorage-with-the-vuex-store

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -11,11 +11,20 @@ if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
+// Selectively cache book data
+store.subscribe((mutation, state) => {
+  let store = { books: state.library.books }
+  localStorage.setItem('store', JSON.stringify(store))
+})
+
 /* eslint-disable no-new */
 /* eslint-disable key-spacing */
 new Vue({
   components: { App },
   router,
   store,
-  template: '<App/>'
+  template: '<App/>',
+  beforeCreate () {
+    this.$store.commit('initialiseStore')
+  }
 }).$mount('#app')
