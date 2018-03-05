ccc0fa98c8511b8477dd3b5500aa8e92e6410ba8
Eric Gardner <gardner.ec@gmail.com>
2018-03-05 08:09:05 -0800

    WIP: Expanding info block in CSS Grid

--- src/renderer/components/Library/LibraryGrid.vue
+++ src/renderer/components/Library/LibraryGrid.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="section">
-    <ul class="library-grid">
-      <li v-for="book in books" class="library-grid__item">
-        <library-item :book="book">
-        </library-item>
-      </li>
-    </ul>
+    <div class="library-grid">
+      <library-item v-for="book in books" :book="book">
+      </library-item>
+    </div>
   </div>
 </template>
 
@@ -36,8 +34,8 @@ $grid-gap: 2.5em;
 
 .library-grid {
   display: grid;
-  grid-template-columns: repeat(auto-fill, 150px);
+  grid-template-columns: repeat(auto-fit, 150px);
   grid-gap: 2rem 3rem;
-  justify-content: center;
+  justify-content: space-between;
 }
 </style>
--- src/renderer/components/Library/LibraryItem.vue
+++ src/renderer/components/Library/LibraryItem.vue
@@ -1,13 +1,25 @@
 <template>
   <div class="library-item">
-    <img alt="" :src="book.thumbnail"/>
+    <a @click="toggleActive">
+      <img alt="" :src="book.thumbnail"/>
+    </a>
+    <div v-if="active" class="library-item__info">
+      <div class="library-item__info__inner">
+        <h2>{{ book.title }}</h2>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name    : 'library-item',
-  props   : ['book'],
+  name : 'library-item',
+  props: ['book'],
+  data () {
+    return {
+      active: false
+    }
+  },
   computed: {
     authorString () {
       if (this.book.authors.length > 1) {
@@ -17,16 +29,39 @@ export default {
         return `By ${this.book.authors[0].name}`
       }
     }
+  },
+  methods: {
+    toggleActive () {
+      this.active = !this.active
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+@import "~bulma";
+
 .library-item {
   line-height: 0;
   
   img {
     box-shadow: 3px 3px 7px 1px rgba(0, 0, 0, 0.3);
-  }    
+  }
+  
+  &__info {
+    grid-column-start: 1;
+    grid-column-end: -1;
+    height: 300px;
+    position: relative;
+    top: 1rem;
+    
+    &__inner {
+      background-color: $light;
+      height: 100%;
+      left: 0;
+      position: absolute;
+      width: calc(100vw - 3rem);
+    }
+  }
 }
 </style>
